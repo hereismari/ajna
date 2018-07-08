c4d0602100b331c55a57151819f0a83fb63a0bee
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-07-08 20:01:28 -0300

    Adding checkpoint saver

--- learning/trainer.py
+++ learning/trainer.py
@@ -12,11 +12,26 @@ class Trainer(object):
         self.exec_name = 'train'
         self.running_losses = {}
 
-    def run_training(self, data, max_steps, eval=True, test=True):
+    def run_training(self, data, max_steps, eval=True, test=True, output_path='checkpoints/cnn.cpkt'):
         self.max_steps = max_steps
+
+        saver = tf.train.Saver()
         with tf.Session() as sess:
             self.initialize_vars(sess)
             self.train(sess, data, eval=eval)
+
+            if output_path is not None:
+                saver.save(sess, output_path)
+                print('Model saved at %s' % output_path)
+
+    def run_eval(self, data, max_steps, output_path='checkpoints/cnn.cpkt'):
+        self.max_steps = max_steps
+
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            saver.restore(sess, output_path)
+
+            self.eval(sess, data)
 
     def initialize_vars(self, sess):
         self.running_loss, self.running_steps = 0, 0
@@ -26,6 +41,12 @@ class Trainer(object):
 
         sess.run(init)
         sess.run(init_l)
+    
+    def eval(self, sess, data):
+        self.data = data
+        data.run(sess)
+        for self.step in range(self.max_steps):
+            self._eval_step(sess, data)
 
     def train(self, sess, data, eval=True):
         self.data = data
@@ -35,7 +56,8 @@ class Trainer(object):
 
     def train_step(self, sess, data, eval=True):
         self.train_batch(sess, data)
-        self._eval_step(sess, data)
+        if eval and (self.step % self.eval_steps == 0) or (self.step + 1 == self.max_steps):
+            self._eval_step(sess, data)
 
     def train_batch(self, sess, data):
         self.model.train(sess)
@@ -47,10 +69,9 @@ class Trainer(object):
         self.running_steps += 1
 
     def _eval_step(self, sess, data):
-        if eval and (self.step % self.eval_steps == 0) or (self.step + 1 == self.max_steps):
-            self.model.eval(sess)
-            self.eval_step(sess, data)
-            data.run(sess)
+        self.model.eval(sess)
+        self.eval_step(sess, data)
+        data.run(sess)
 
     def eval_step(self, sess, data):
         s = ''
